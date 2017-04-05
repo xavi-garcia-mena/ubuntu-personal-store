39e0d86693e8c0dc07f9dda648ac50c300649bb8
xavi-garcia-mena <xavi-garcia-mena@canonical.com>
2017-04-05 13:57:30 +0200

    fix statement when removing encrypted id_rsa, try 2

--- tests/setup-deploy-key.sh
+++ tests/setup-deploy-key.sh
@@ -3,7 +3,7 @@
 set -e
 
 openssl aes-256-cbc -K $encrypted_527ca8567c65_key -iv $encrypted_527ca8567c65_iv -in travis/id_rsa_snapwebci_enc -out id_rsa_snapwebci -d
-rm id_rsa_snapwebci_enc
+rm travis/id_rsa_snapwebci_enc
 chmod 600 id_rsa_snapwebci; mv id_rsa_snapwebci ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 ssh-keyscan -t ecdsa $host 2>&1 | tee -a $HOME/.ssh/known_hosts
